7cf12e555b9e1214c24a06cca19b0d883719986d
Shanib c k <ckshanib8@gmail.com>
2023-06-17 21:30:58 +0530

    button add and action add in javascript

---
+++ bulb light on and off/node.js
@@ -0,0 +1,7 @@
+function bulbOn() {
+    document.getElementById("bulb-el").src="images/pic_bulbon.gif"
+}
+
+function bulbOff() {
+    document.getElementById("bulb-el").src="images/pic_bulboff.gif"
+}
